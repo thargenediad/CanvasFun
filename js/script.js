8a9835bda12866423d74c9919ccfc0aae86009c7
atthorn <anton.t.thorn@nasa.gov>
2014-07-23 22:33:29 -0500

    added Fabric.js and got a basic Hello World demo going with it; added soyuz image

--- js/script.js
+++ js/script.js
@@ -2,8 +2,23 @@
  * Created by atthorn on 7/18/2014.
  */
 $(document).ready(function() {
-  draw();
-  onresize = draw;
+  //draw();
+  //onresize = draw;
+
+
+  var flightSeqenceCanvas = $("#flightSequenceCanvas")[0];
+  var canvasWidth = flightSeqenceCanvas.width = flightSeqenceCanvas.clientWidth;
+  var canvasHeight = flightSeqenceCanvas.height = flightSeqenceCanvas.clientHeight;
+  var radius = (canvasWidth > canvasHeight) ? canvasHeight / 2 : canvasWidth / 2; // set the radius of the circle to be the lesser of the width or height;
+
+  // create a wrapper around native canvas element (with id="flightSequenceCanvas")
+  var canvas = new fabric.Canvas("flightSequenceCanvas");
+
+  var circle = new fabric.Circle({
+    radius: radius, fill: 'green', left: canvasWidth / 2, top: canvasHeight / 2, originX: 'center', originY: 'center'
+  });
+
+  canvas.add(circle);
 });
 
 var draw = function() {
